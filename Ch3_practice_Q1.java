8ede18abaf296164f81744be500b0e2975dc8553
Kaushal kumar <141163718+Kaushalmehta18@users.noreply.github.com>
2024-03-30 22:37:47 +0530

    Add files via upload

---
+++ Ch3_practice_Q1.java
@@ -0,0 +1,7 @@
+// WAP to convert a string into lower case
+public class Ch2_practice_Q1 {
+    public static void main(String[] args) {
+        String name = "KAUSHAL";
+        System.out.println(name.toLowerCase());
+    }
+}
