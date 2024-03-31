b51fcf68c4ed9bc2b185721de5b5d15948a87dd0
Kaushal kumar <141163718+Kaushalmehta18@users.noreply.github.com>
2024-03-31 11:54:58 +0530

    Add files via upload

---
+++ Ch3_practice_Q2.java
@@ -0,0 +1,7 @@
+// WAP to repalce space with undercores
+public class Ch2_practice_Q2 {
+    public static void main(String[] args) {
+        String str = "Kaushal is good boy";
+        System.out.println(str.replace(" ", "_"));
+    }
+}
