e3d45680e323c86b38fac375463953a58963d242
Kaushal kumar <141163718+Kaushalmehta18@users.noreply.github.com>
2024-03-31 11:56:22 +0530

    Add files via upload

---
+++ Ch3_practice_Q5.java
@@ -0,0 +1,6 @@
+// WAP to format the string --> "Dear Kaushal , This Java course is nice. Thnaks"
+public class Ch2_practice_Q5 {
+    public static void main(String[] args) {
+        System.out.println("\" Dear Kaushal , This Java course is nice. Thnaks \" ");
+    }
+}
