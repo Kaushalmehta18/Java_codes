a1455cad2b6c020f2be8c93ea0722a67fb20a2e5
Kaushal kumar <141163718+Kaushalmehta18@users.noreply.github.com>
2024-04-04 23:55:34 +0530

    Add files via upload

---
+++ Ch5_practice_Q1.java
@@ -0,0 +1,11 @@
+public class Ch5_practice_Q1 {
+    public static void main(String[] args) {
+        for(int i=5;i>0;i--){
+            for(int j=5;j>i;j--){
+                System.out.print("*****");
+            }
+            System.out.println("");
+        }
+        
+    }
+}
